344b29a4ca1d192f04296a696a55102ffe747875
Daniel Lappe <daniellappe@icloud.com>
2024-07-06 16:26:56 +0200

    implemented login/logout functionality

--- src/book_rec_system.py
+++ src/book_rec_system.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import hashlib
-
+import time
+import networkx as nx
 
 
 class Book:
@@ -37,12 +38,15 @@ class User:
         User.next_id += 1
         self.alias = alias
         self.password_hash = self.hash_password(password)
-        self.favourite_books = {}
+        self.favourite_book = {}
+        self.favourite_quote = None
+        self.favourite_author = None
         self.rated_books = {}
         self.current_book = {}
         self.read_books = {}
-        self.users = {}
-        self.users[self.user_id] = self.alias
+        self.friends = []
+        self.users_backup = {}
+        self.users_backup[self.user_id] = self.alias
 
     @staticmethod
     def hash_password(password):
@@ -60,40 +64,126 @@ class User:
             rated_books_str += f"{book} -> {rating}\n"
         return rated_books_str
     
+    def add_friend(self, friend):
+        self.friends.append(friend)
+
 class UserDatabase:
     def __init__(self):
         self.users = {}
+        self.current_user = None
+        self.logged_in = False
+        self.graph = nx.Graph()
 
     def add_user(self, user):
-        self.users[user.user_id] = user
+        self.users[user.alias] = user
 
-    def create_profile(user_db):
+    def create_profile(self):
         first_n = input("What's your first name? ")
         last_n = input("What's your last name? ")
         while True:
-            nickn = input("What would you like your nickname to be? ")
-            if nickn not in user_db.users:
+            alias = input("What would you like your nickname to be? ")
+            if alias not in self.users:
                 break
             print("This nickname is already taken. Please choose another one.") 
-        password = input("Enter your password: ")
-        new_user = User(first_n, last_n, nickn, password)
-        user_db.add_user(new_user)
+        password = input("Choose your password: ")
+        new_user = User(first_n, last_n, alias, password)
+        new_user.nickname = alias
+        self.add_user(new_user)
+        self.graph.add_node(new_user.user_id)
         print("Your profile has been created successfully! Another bookworm joined the team!")
-        return nickn
+        return alias
 
-    def login(user_db, alias=None):
+    def login(self, alias=None):
+        counter = 0
         if alias is None:
             while True:
                 alias = input("Enter your nickname: ")
-                if alias in user_db.users:
+                if alias in self.users:
                     break
                 print("This nickname is not in our database. Please try again.")
         while True:
             password = input("Enter your password: ")
-            if user_db.users[alias].check_password(password):
+            counter += 1
+            if self.users[alias].check_password(password):
                 print("You have successfully logged in!")
+                self.current_user = self.users[alias]
+                self.logged_in = True
                 break
-            print("Invalid password. Please try again.")
+            if counter < 3:
+                print("Invalid password. Please try again.")
+            else:
+                raise LoginFailedException
+    
+    def logout(self):
+        self.current_user = None
+        self.logged_in = False
+        print("You have successfully logged out.")
+
+    def add_friendship(self, user_alias, friend_alias):
+        user = self.users[user_alias]
+        friend = self.users[friend_alias]
+        user.add_friend(friend)
+        self.graph.add_edge(user_alias, friend_alias)
+
+    def see_profile(self):
+        user = self.current_user
+        print(f"Name: {user.first_name} {user.last_name}")
+        print(f"Alias: {user.alias}")
+        print(f"Favorite Book: {user.favourite_book}")
+        print(f"Favorite Quote: {user.favourite_quote}")
+        print(f"Favorite Author: {user.favourite_author}")
+        print(f"Current Book: {user.current_book}")
+        print("Friends: ")
+        for friend in user.friends:
+            print(friend.alias) 
+
+    def edit_profile(self):
+        while True:
+            action = input("What would you like to change? Your favourite book, quote or author? What book you are currently reading? Or your friendslist? Or the books you read so far? Or do you want exit? Type \"favourite book\", \"favourite quote\", \"favourite author\", \"current book\", \"friendslist\", \"books read\", \"exit\" ")
+            if action.lower() == "favourite book":
+                new_book_title = input("Enter the title of your new favourite book: ")
+                new_book_author = input("Enter the author of your new favourite book: ")
+                new_book_publication_year = input("Enter the publication year of your new favourite book: ")
+                new_book_genre = input("Enter the genre of your new favourite book: ")
+                self.current_user.favourite_book = {
+                    "title": new_book_title,
+                    "author": new_book_author,
+                    "publication_year": new_book_publication_year,
+                    "genre": new_book_genre
+                }
+            elif action.lower() == "favourite quote":
+                new_quote = input("Enter your new favourite quote: ")
+                self.current_user.favourite_quote = new_quote
+            elif action.lower() == "favourite author":
+                new_author = input("Enter your new favourite author: ")
+                self.current_user.favourite_author = new_author
+            elif action.lower() == "current book":
+                new_book = input("Enter the book you are currently reading: ")
+                self.current_user.current_book = new_book
+            elif action.lower() == "friendslist":
+                while True:
+                    new_friend_alias = input("Enter the alias of the new friend you want to add, or type 'exit' to cancel: ")
+                    if new_friend_alias.lower() == "exit":
+                        print("Action cancelled.")
+                        break
+                    elif new_friend_alias in self.users:
+                        self.add_friendship(self.current_user.alias, new_friend_alias)
+                        break
+                    else:
+                        print("This user does not exist. Please try again.")
+            elif action.lower() == "books read":
+                new_book_title = input("Enter the title of a book you have read: ")
+                new_book_author = input("Enter the author of the book you have read: ")
+                self.current_user.read_books[new_book_title] = new_book_author
+            elif action.lower() == "exit":
+                break
+            else:
+                print("Invalid choice. Please try again.")
+
+
+
+class LoginFailedException(Exception):
+    pass
 
 class Rating:
     def __init__(self, user, book, score, timestamp):
--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@
 
 import os
 from colorama import Fore, Style
-from book_rec_system import Book, User, Library, Rating, RatingSystem, RecomEngine, UserDatabase
+from book_rec_system import Book, User, Library, Rating, RatingSystem, RecomEngine, UserDatabase, LoginFailedException
+import time
 
 # Clear the terminal screen
 
@@ -31,21 +32,41 @@ print("============================================")
 # Main loop
 
 def main():
-    pass
-
-#1: Create a user profile: request personal data, desired nickname...
-    while True:
-        signup_or_in = input("Would you like to log in or create a profile? Type \"login\" or \"create profile\" ")
         user_db = UserDatabase()
 
+        while True:
+            if user_db.logged_in:
+                action = input("Would you like to see your profile, edit your profile, log out, or exit? Type \"see profile\", \"edit profile\", \"logout\", or \"exit\" ")
+                if action.lower() == "see profile":
+                    user_db.see_profile() 
+                elif action.lower() == "edit profile":
+                    user_db.edit_profile()  
+                elif action.lower() == "logout":
+                    user_db.logout()
+                elif action.lower() == "exit":
+                    break
+                else:
+                    print("Invalid choice. Please try again.")
+            else:
+                action = input("Would you like to log in, create a profile, or exit? Type \"login\", \"create profile\", or \"exit\" ")
+                if action.lower() == "create profile":
+                    user_db.create_profile()
+                elif action.lower() == "login":
+                    try:
+                        user_db.login()
+                    except LoginFailedException:
+                        print("You tried to log in too many times. Please wait for 10 seconds.")
+                        for i in range(10, 0, -1):
+                            print(i)
+                            time.sleep(1)
+                elif action.lower() == "exit":
+                    break
+                else:
+                    print("Invalid choice. Please try again.")
 
-        if signup_or_in.lower() == "create profile":
-            alias = user_db.create_profile()
-            user_db.login(alias)
-        elif signup_or_in.lower() == "login":
-            user_db.login(user_db)
-        else:
-            print("Invalid choice. Please try again.")
+
+#1: Create a user profile: request personal data, desired nickname...
+    
 
 
 
